6a94c6d877b8e9a442f37c67d6612f4ca63a9074
basilyazbeck290-cmyk <basilyazbeck290@gmail.com>
2026-01-15 11:48:49 +0100

    Update app.py

--- app.py
+++ app.py
@@ -61,7 +61,7 @@ if st.button("Voir la surprise 🎁"):
         time.sleep(0.035) # Un tout petit peu plus lent pour le suspense
         my_bar.progress(percent_complete + 1, text=progress_text)
 
-    time.sleep(5)
+    time.sleep(1)
     
     my_bar.empty() # On efface la barre
     
