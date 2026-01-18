802269b10249d0c05762ced00eb8d59f6b1dc4d4
basilyazbeck290-cmyk <basilyazbeck290@gmail.com>
2026-01-18 16:39:21 +0100

    Update surprise.py

--- surprise.py
+++ surprise.py
@@ -126,7 +126,7 @@ with col1:
     }
     
     info = diags[batterie]
-    couleurs_douces = {"error": "#FF4B4B", "warning": "#FFA421", "info": "#00C0F2", "success": "#00D488", "success2": "#06402B"}
+    couleurs_douces = {"error": "#FF4B4B", "warning": "#FFA421", "info": "#00C0F2", "success": "#00D488", "success2": "#466D1D"}
     color = couleurs_douces.get(info['c'], "#FFFFFF")
 
     st.markdown(f"""
